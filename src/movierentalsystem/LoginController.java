88c21d02e7c5403d82477f50d7be78d38cf94884
enun03 <enunez025@gmail.com>
2018-11-07 21:13:27 -0400

    Pupulating TableView with data from MySQL db

--- src/movierentalsystem/LoginController.java
+++ src/movierentalsystem/LoginController.java
@@ -10,23 +10,40 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 /**
  *
  * @author Eli Núñez
  */
-public class FXMLDocumentController implements Initializable {
+public class LoginController implements Initializable {
     
     @FXML
+    private Button button;
+
+    @FXML
     private Label label;
+
+    @FXML
+    private TableView<DataTableModel> tblData;
+
+    @FXML
+    private TableColumn<DataTableModel, String> col_id;
+
+    @FXML
+    private TableColumn<DataTableModel, String> col_value;
+     
+    ObservableList<DataTableModel> obDataTableList = FXCollections.observableArrayList();
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -62,6 +79,33 @@ public class FXMLDocumentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        MySQLConnection conn = new MySQLConnection();
+        String sql = "SELECT * FROM `test`";
+        
+        
+        
+        PreparedStatement statement;
+        try {
+            statement = conn.connect().prepareStatement(sql);
+        
+            ResultSet rs = statement.executeQuery();
+            
+            while(rs.next()) {
+                obDataTableList.add(new DataTableModel(
+                    Integer.toString(rs.getInt("id")),
+                    rs.getString("value")
+                ));
+            }
+            
+            rs.close();
+            statement.close();
+            
+            col_id.setCellValueFactory(new PropertyValueFactory<>("id"));
+            col_value.setCellValueFactory(new PropertyValueFactory<>("value"));
+            
+            tblData.setItems(obDataTableList);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
-    
 }
