467f614a5a5f919fc328875b9c6419ade7917323
enun03 <enunez025@gmail.com>
2018-11-08 20:57:44 -0400

    Navigation | EventHandeling

---
+++ src/movierentalsystem/Controllers/EditTableDataController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movierentalsystem.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Eli Núñez
+ */
+public class EditTableDataController implements Initializable {
+    @FXML
+    private TextField txtValue;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private Label lblID;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+    }    
+    
+}
--- src/movierentalsystem/Controllers/LoginController.java
+++ src/movierentalsystem/Controllers/LoginController.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package movierentalsystem;
+package movierentalsystem.Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ArrayList;
@@ -16,12 +17,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import movierentalsystem.Models.DataTableModel;
+import movierentalsystem.MySQLConnection;
 /**
  *
  * @author Eli Núñez
@@ -42,8 +49,37 @@ public class LoginController implements Initializable {
 
     @FXML
     private TableColumn<DataTableModel, String> col_value;
+    
+    @FXML
+    private Button btnEditTableData;
      
     ObservableList<DataTableModel> obDataTableList = FXCollections.observableArrayList();
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {    
+        // Init EventHandlers
+        // On edit button click
+        btnEditTableData.setOnAction((e) -> {
+            Stage stage = null;
+            Parent myNewScene = null;
+            try {
+                stage = (Stage) btnEditTableData.getScene().getWindow();            
+                myNewScene = FXMLLoader.load(getClass().getResource("EditTableData.fxml"));
+                
+                Scene scene = new Scene(myNewScene);
+                stage.setScene(scene);
+                stage.setTitle("My New Scene");
+                stage.show();
+            } catch (IOException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+        
+        // Fetching Table Data
+        getTableData();
+    }  
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -76,18 +112,13 @@ public class LoginController implements Initializable {
         }
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void getTableData() {
         // TODO
         MySQLConnection conn = new MySQLConnection();
         String sql = "SELECT * FROM `test`";
         
-        
-        
-        PreparedStatement statement;
         try {
-            statement = conn.connect().prepareStatement(sql);
-        
+            PreparedStatement statement = conn.connect().prepareStatement(sql);
             ResultSet rs = statement.executeQuery();
             
             while(rs.next()) {
@@ -104,8 +135,9 @@ public class LoginController implements Initializable {
             col_value.setCellValueFactory(new PropertyValueFactory<>("value"));
             
             tblData.setItems(obDataTableList);
+            
         } catch (SQLException ex) {
             Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }    
+    }
 }
